d9f1c7d5ffe7293a776d0d9593300d71c03c226e
ematveev <evgen.keygen@gmail.com>
2020-01-26 11:40:58 +0300

    add run script for Windows, refactor some code

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello from React</h1>
-  );
+import AppHeader from './app-header';
+import RegisterPage from './pages/register-page';
+import LoginPage from './pages/login-page';
+import AppFooter from './app-footer';
+
+import '../styles/app.css';
+import PrivateRoute from './private-router';
+import UserPage from './pages/user-page';
+import Auth from './hoc-helpers/with-auth';
+
+export default class App extends Component {
+  render() {
+
+      return (
+          <BrowserRouter>
+              <Auth>
+                <AppHeader/>
+                  <Switch>
+                      <PrivateRoute path={"/user"} component={UserPage}/>
+                      <Route path="/"
+                         render={() => <h2>Welcome to FriendsDeliver</h2>}
+                         exact />
+                      <Route path="/register" component={RegisterPage}/>
+                      <Route path="/login" render={() => (
+                          <LoginPage/>)}/>
+                      <Route render={() => <h2>Page not found</h2>} />
+                  </Switch>
+                <AppFooter/>
+              </Auth>
+          </BrowserRouter>
+      );
+  }
 }
-
-export default App;
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, Response, jsonify, request, abort
-from webapp.model import db, User, Station, UserStation 
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from webapp.model import db, User, Station, UserStation
 from marshmallow import Schema, fields
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    jwt = JWTManager(app)
+    CORS(app)
     return app  
 
 app = create_app()
@@ -26,18 +30,53 @@ def user_exists(user_id):
     users_list = User.query.filter(User.id == user_id).all()
     return len(users_list)>0
 
-@app.route('/hi')
+@app.route('/')
 def index():
-    return 'hi'
+    return 'OK'
+
+@app.route('/registration', methods=['POST'])
+def new_user():
+    json_data = request.json
+    name = json_data['name']
+    email = json_data['email']
+    password = json_data['password']
+    delivers = json_data['delivers']
+
+    if User.query.filter_by(email=email).first() is None:
+        user = User(name=name, email=email, delivers=delivers)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'id': user.id, 'name': user.name}), 201
+
+    return jsonify({'msg': "Something got wrong"}), 400
+
+@app.route('/login', methods=['POST'])
+def login():
+    json_data = request.json
+    email = json_data['email']
+    password = json_data['password']
+    if not email:
+        return jsonify({'msg': "Missing user email parameter"}), 400
+    if not password:
+        return jsonify({'msg': "Missing password parameter"}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user and user.check_password(password) == True:
+        access_token = create_access_token(identity=user.email)
+        return jsonify(access_token=access_token), 200
+
+    return jsonify({"msg": "Bad username or password"}), 400
 
 @app.route('/v1/stations/')
-def stations():    
+def stations():
     stations_list = Station.query.all()
     schema = StationSchema(many=True)
     result = schema.dump(stations_list)
     return jsonify(result)
 
 @app.route('/v1/stations/users/')
+@jwt_required
 def users_stations():
     station_from = request.args.get('from', type = int)
     station_to = request.args.get('to', type = int)
@@ -62,7 +101,7 @@ def users_stations():
     result = schema.dump(users_list)
     return jsonify(result)
 
-@app.route('/v1/users/<user_id>/stations/') 
+@app.route('/v1/users/<user_id>/stations/')
 def get_station(user_id):
     if not user_exists(user_id):
         abort(404)
